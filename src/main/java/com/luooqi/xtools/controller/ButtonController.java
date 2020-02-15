71cfd51089971aac115a76bcc367af2cea349f09
AnyListen <heheipi@qq.com>
2020-02-15 16:12:42 +0800

    new ui designer

---
+++ src/main/java/com/luooqi/xtools/controller/ButtonController.java
@@ -0,0 +1,8 @@
+package com.luooqi.xtools.controller;
+
+import io.datafx.controller.ViewController;
+
+@ViewController(value = "/fxml/ui/Button.fxml", title = "Material Design Example")
+public class ButtonController {
+
+}
