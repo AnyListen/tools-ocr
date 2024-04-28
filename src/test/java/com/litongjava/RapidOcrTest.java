38de48be7d2f1609b278484d151202d3ef122c05
litongjava <litongjava@qq.com>
2024-04-28 06:20:04 -1000

    set default thread is 2

--- src/test/java/com/litongjava/RapidOcrTest.java
+++ src/test/java/com/litongjava/RapidOcrTest.java
@@ -4,11 +4,14 @@ import com.benjaminwan.ocrlibrary.OcrResult;
 
 import io.github.mymonstercat.Model;
 import io.github.mymonstercat.ocr.InferenceEngine;
+import io.github.mymonstercat.ocr.config.HardwareConfig;
 
 public class RapidOcrTest {
   public static void main(String[] args) {
-    InferenceEngine engine = InferenceEngine.getInstance(Model.ONNX_PPOCR_V4_SERVER);
-    OcrResult ocrResult = engine.runOcr("images/01.png");
+    HardwareConfig onnxConfig = HardwareConfig.getOnnxConfig();
+    onnxConfig.setNumThread(2);
+    InferenceEngine engine = InferenceEngine.getInstance(Model.ONNX_PPOCR_V4_SERVER, onnxConfig);
+    OcrResult ocrResult = engine.runOcr("C:\\Users\\Administrator\\Desktop\\01.jpg");
     System.out.println(ocrResult.getStrRes().trim());
   }
 }
